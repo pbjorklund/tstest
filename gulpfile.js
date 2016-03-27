f14e6dea8eb1cc3835d456d3e03a4826b2556b5b
Patrik Björklund <patrik@cognit.se>
2016-03-27 21:06:34 +0200

    Add browsersync and a html file to run some code against the browser

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require("gulp"),
     print = require('gulp-print'),
     src = require("vinyl-source-stream"),
     mocha = require("gulp-mocha"),
-    ts = require("gulp-typescript");
+    ts = require("gulp-typescript"),
+    browserSync = require('browser-sync').create();
 
 gulp.task('build', function() {
     return gulp.src('src/**/*.ts')
@@ -17,6 +18,23 @@ gulp.task("test", ["build"], function () {
         .pipe(mocha({ui: 'bdd', reporter: 'dot'}));
 });
 
-gulp.task("watch", function(){                                   
-    gulp.watch(["./src/**/*.ts"], ["build", "test"]);       
-});                                                              
+gulp.task("watch", function(){
+    gulp.watch(["./src/**/*.ts"], ["build", "test"]);
+});
+
+// NOTE: Wrap browserSync.reload in an anon func for it to
+//       not stop after the first trigger
+gulp.task('js-watch', ['build'], function() { browserSync.reload() });
+
+gulp.task('sync', ['build'], function () {
+    browserSync.init({
+        server: {
+            baseDir: "./server",
+            routes: {
+                "/build": "build"
+            }
+        }
+    });
+
+    gulp.watch(["./src/**/*.ts"], ["js-watch"]);
+});
---
+++ src/browser.ts
@@ -0,0 +1,1 @@
+document.getElementById("#divOne").innerHTML = "<h1>Simple execution of browser code</h1>";
